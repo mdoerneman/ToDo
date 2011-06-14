254d75a283a6261c5ff0aee00d5dd0bd37a833b2
Michael Doerneman <mdoerneman@gmail.com>
2011-06-14 06:26:10 -0500

    minor change and route change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,6 +12,13 @@ class PagesController < ApplicationController
     resp = RestClient.get(url + "getProjects", :accept => :json, :params => params)
     @projects = JSON.parse(resp.body)
 
+
+    params[:project_id] = @projects[0]["id"]
+    resp = RestClient.get(url + "getUncompletedItems", :accept => :json, :params => params)
+    arr = JSON.parse(resp.body)
+    @tasks.push(arr[rand(arr.size)]) unless arr.empty?
+
+
     group_tasks = []
     @projects.each do |p|
       if p["item_order"] > 2
@@ -22,10 +29,7 @@ class PagesController < ApplicationController
     end
     @tasks.push(group_tasks[rand(group_tasks.size)])  unless group_tasks.empty?
 
-    params[:project_id] = @projects[0]["id"]
-    resp = RestClient.get(url + "getUncompletedItems", :accept => :json, :params => params)
-    arr = JSON.parse(resp.body)
-    @tasks.push(arr[rand(arr.size)]) unless arr.empty?
+
 
     params[:project_id] = @projects[1]["id"]
     resp = RestClient.get(url + "getUncompletedItems", :accept => :json, :params => params)
